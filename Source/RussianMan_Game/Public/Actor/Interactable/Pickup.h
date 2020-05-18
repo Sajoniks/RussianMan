a4a36c226f4ce60ad45907e2bd69f2d1a0f625a7
Sajoniks <avesom33@mail.ru>
2020-05-18 18:15:44 +0300

    Added pickup and item stack

---
+++ Source/RussianMan_Game/Public/Actor/Interactable/Pickup.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actor/Interactable/InteractableBase.h"
+#include "Pickup.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RUSSIANMAN_GAME_API APickup : public AInteractableBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/RussianMan_Game/Public/Data/ItemsRegistry.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "ItemsRegistry.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RUSSIANMAN_GAME_API UItemsRegistry : public UDataAsset
+{
+	GENERATED_BODY()
+	
+};
