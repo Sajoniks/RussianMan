1bd756360c28de844287747fac6efe80d72e0442
Sajoniks <avesom33@mail.ru>
2020-05-20 13:30:55 +0300

    Removing functions added

--- Source/RussianMan_Game/Private/Components/InventoryComponent.cpp
+++ Source/RussianMan_Game/Private/Components/InventoryComponent.cpp
@@ -2,6 +2,7 @@
 
 
 #include "Components/InventoryComponent.h"
+#include "RussianMan_Game/RussianMan_Game.h"
 
 // Sets default values for this component's properties
 UInventoryComponent::UInventoryComponent()
@@ -13,6 +14,138 @@ UInventoryComponent::UInventoryComponent()
 	// ...
 }
 
+void UInventoryComponent::AddStack(FItemStack& Stack)
+{
+	UE_LOG(LogInventory, Warning, TEXT("Can't add items to container, %s, ignoring"), *GetOwner()->GetName());
+}
+
+bool UInventoryComponent::RemoveStack(int32 Count, const FGameplayTag& ID)
+{
+	if (Num > 0)
+	{
+		const auto Stack = Inventory.FindByPredicate([&ID](const FItemStack& Stack)
+		{
+			return Stack == ID;
+		});
+
+		if (Stack)
+		{
+			const int32 RemoveCount = FMath::Clamp(Count, 0, Stack->Num);
+
+			if (RemoveCount == Stack->Num)
+			{
+				--Num;
+				*Stack = FItemStack::EmptyStack;
+			}
+			else
+				Stack->Num -= RemoveCount;
+
+			return true;
+		}
+	}
+	return false;
+}
+
+bool UInventoryComponent::RemoveStack(int32 Count, const FItemStack& Stack)
+{
+	return RemoveStack(Count, Stack.ID);
+}
+
+bool UInventoryComponent::RemoveStack(int32 Count, int32 Idx)
+{
+	if (Num > 0)
+	{
+		if (Inventory.IsValidIndex(Idx))
+		{
+			FItemStack& Stack = Inventory[Idx];
+
+			const int32 RemoveCount = FMath::Clamp(Count, 0, Stack.Num);
+
+			if (RemoveCount == Stack.Num)
+			{
+				Stack = FItemStack::EmptyStack;
+				--Num;
+			}
+			else
+				Stack.Num -= RemoveCount;
+
+			return true;
+		}
+	}
+	return false;
+}
+
+bool UInventoryComponent::RemoveStack(const FGameplayTag& ID)
+{
+	if (Num > 0)
+	{
+		const auto Stack = Inventory.FindByPredicate([&ID](const FItemStack& Stack)
+		{
+			return Stack == ID;
+		});
+
+		if (Stack)
+		{
+			*Stack = FItemStack::EmptyStack;
+			--Num;
+			return true;
+		}
+	}
+	return false;
+}
+
+bool UInventoryComponent::RemoveStack(const FItemStack& Stack)
+{
+	return RemoveStack(Stack.ID);
+}
+
+bool UInventoryComponent::RemoveStack(int32 Idx)
+{
+	if (Num > 0)
+	{
+		if (Inventory.IsValidIndex(Idx))
+		{
+			Inventory[Idx] = FItemStack::EmptyStack;
+			--Num;
+			return true;
+		}
+	}
+	return false;
+}
+
+bool UInventoryComponent::RemoveAll(const FGameplayTag& ID)
+{
+	if (Num > 0)
+	{
+		bool bRemoved = false;
+		
+		auto Stack = Inventory.FindByPredicate([&ID](const FItemStack& Stack)
+		{
+			return Stack == ID;
+		});
+
+		while (Stack && Num > 0)
+		{
+			bRemoved = true;
+			--Num;
+			*Stack = FItemStack::EmptyStack;
+
+			Stack = Inventory.FindByPredicate([&ID](const FItemStack& Stack)
+			{
+				return Stack == ID;
+			});
+		}
+
+		return bRemoved;
+	}
+
+	return false;
+}
+
+bool UInventoryComponent::RemoveAll(const FItemStack& Stack)
+{
+	return RemoveAll(Stack.ID);
+}
 
 // Called when the game starts
 void UInventoryComponent::BeginPlay()
--- Source/RussianMan_Game/Public/Components/InventoryComponent.h
+++ Source/RussianMan_Game/Public/Components/InventoryComponent.h
@@ -4,6 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "Data/ItemStack.h"
+
 #include "InventoryComponent.generated.h"
 
 
@@ -12,17 +14,42 @@ class RUSSIANMAN_GAME_API UInventoryComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
+	float Weight;
+	uint32 Num;
+
 public:	
 	// Sets default values for this component's properties
 	UInventoryComponent();
 
+	virtual void AddStack(FItemStack& Stack);
+	
+	virtual bool RemoveStack(int32 Count = 1, const FGameplayTag& ID);
+	virtual bool RemoveStack(int32 Count = 1, const FItemStack& Stack);
+	virtual bool RemoveStack(int32 Count = 1, int32 Idx);
+
+	virtual bool RemoveStack(const FGameplayTag& ID);
+	virtual bool RemoveStack(const FItemStack& Stack);
+	virtual bool RemoveStack(int32 Idx);
+	
+	virtual bool RemoveAll(const FGameplayTag& ID);
+	virtual bool RemoveAll(const FItemStack& Stack);
+	
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
+
+	//Array of items
+	TArray<FItemStack> Inventory;
 
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
+	const TArray<FItemStack>& GetInventoryArray() const;
+
 };
+
+inline const TArray<FItemStack>& UInventoryComponent::GetInventoryArray() const
+{
+	return Inventory;
+}
