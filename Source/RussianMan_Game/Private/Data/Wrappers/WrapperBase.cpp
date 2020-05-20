000885891474c7591a34266066415b6ccd1cc6d1
Sajoniks <avesom33@mail.ru>
2020-05-20 12:05:57 +0300

    Basic wrapper interface

--- Source/RussianMan_Game/Private/Data/Wrappers/WrapperBase.cpp
+++ Source/RussianMan_Game/Private/Data/Wrappers/WrapperBase.cpp
@@ -3,3 +3,18 @@
 
 #include "Data/Wrappers/WrapperBase.h"
 
+void UWrapperBase::ClearStackReference()
+{
+	*WrapStack = FItemStack::EmptyStack;
+	WrapStack = nullptr;
+}
+
+void UWrapperBase::GetStack(FItemStack& Stack) const
+{
+	Stack = *WrapStack;
+}
+
+void UWrapperBase::SetStack(FItemStack& Stack)
+{
+	WrapStack = &Stack;
+}
--- Source/RussianMan_Game/Public/Data/Wrappers/WrapperBase.h
+++ Source/RussianMan_Game/Public/Data/Wrappers/WrapperBase.h
@@ -4,14 +4,42 @@
 
 #include "CoreMinimal.h"
 #include "UObject/NoExportTypes.h"
+#include "Data/ItemStack.h"
 #include "WrapperBase.generated.h"
 
+struct FItemStack;
 /**
  * 
  */
-UCLASS()
+UCLASS(Abstract, Blueprintable)
 class RUSSIANMAN_GAME_API UWrapperBase : public UObject
 {
 	GENERATED_BODY()
+
+	//Pointer to wrapped stack
+	FItemStack* WrapStack;
+
+	/**Clears containing stack (sets to null)
+	 * @note Should be called at the end of Unwrap function, after calling of it, function GetStack() mustn't be called!!
+	 */
+	UFUNCTION(BlueprintCallable, Category="Interaction|Pickup|Utility")
+	void ClearStackReference();
+
+	/**Return containing stack reference
+	 * @param Wrapped stack
+	 * @note Mustn't be called after ClearStackReference() function call!!
+	 */
+	UFUNCTION(BlueprintCallable, Category="Interaction|Pickup|Utility")
+	void GetStack(FItemStack& Stack) const;
 	
+public:
+
+	/**Calls corresponding to stack function (eg. add weapon for weapons)
+	 * @param Caller Where to add stack
+	 */
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent, Category="Interaction|Pickup|Wrapper")
+	bool Unwrap(class ARussianCharacter* Caller);
+
+	//Sets wrapper stack to this wrapper
+	void SetStack(FItemStack& Stack);
 };
