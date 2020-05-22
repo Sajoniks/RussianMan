b52433bf3a8e1fc229058064e38cfe4a4b4eeb1b
Sajoniks <avesom33@mail.ru>
2020-05-22 19:25:33 +0300

    Update InventoryComponent.h

--- Source/RussianMan_Game/Public/Components/InventoryComponent.h
+++ Source/RussianMan_Game/Public/Components/InventoryComponent.h
@@ -8,43 +8,117 @@
 
 #include "InventoryComponent.generated.h"
 
+enum class EFillType : uint8
+{
+	Free,
+	Partial
+};
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class RUSSIANMAN_GAME_API UInventoryComponent : public UActorComponent
 {
 	GENERATED_BODY()
-
-	float Weight;
-	uint32 Num;
-
+	
 public:	
 	// Sets default values for this component's properties
 	UInventoryComponent();
 
-	virtual void AddStack(FItemStack& Stack);
-	
-	virtual bool RemoveStack(int32 Count = 1, const FGameplayTag& ID);
-	virtual bool RemoveStack(int32 Count = 1, const FItemStack& Stack);
-	virtual bool RemoveStack(int32 Count = 1, int32 Idx);
+	/**Spreads given stack across inventory and fills with it free slots
+	 * @param Stack Stack to add
+	 */
+	UFUNCTION(BlueprintCallable, Category="Inventory|Action")
+	bool AddStack(UPARAM(ref)FItemStack& Stack);
 
-	virtual bool RemoveStack(const FGameplayTag& ID);
-	virtual bool RemoveStack(const FItemStack& Stack);
-	virtual bool RemoveStack(int32 Idx);
+	/**Removes given count of given item ID
+	 * @param Count Count to remove
+	 * @param ID Item ID
+	 * @return true, if item was removed
+	 */
+	bool RemoveStack(int32 Count, const FGameplayTag& ID);
 	
-	virtual bool RemoveAll(const FGameplayTag& ID);
-	virtual bool RemoveAll(const FItemStack& Stack);
+	/**Removes given count of given item stack
+	 * @param Count Count to remove
+	 * @param Stack Item stack
+	 * @return true, if item was removed
+	 */
+	bool RemoveStack(int32 Count, const FItemStack& Stack);
+
+	/**Removes given count at given inventory index
+	 * @param Count Count to remove
+	 * @param Idx Inventory index
+	 * @return true, if item was removed
+	 */
+	bool RemoveStack(int32 Count, int32 Idx);
+
+	/**Removes one item slot with given ID
+	 * @param ID Item ID
+	 * @return true, if item was removed
+	 */
+	bool RemoveStack(const FGameplayTag& ID);
+
+	/**Removes one item slot with given stack
+	 * @param Stack Item stack
+	 * @return true, if item was removed
+	 */
+	bool RemoveStack(const FItemStack& Stack);
+
+	/**Removes one item slot with given index
+	 * @param Idx Inventory index
+	 * @return true, if item was removed
+	 */
+	bool RemoveStack(int32 Idx);
+
+	/**Completely wipes items from inventory with given ID
+	 * @param ID Item ID
+	 * @return true, if there was at least one removal 
+	 */
+	bool RemoveAll(const FGameplayTag& ID);
+
+	/**Completely wipes items from inventory with given stack
+	 * @param Stack Item stack
+	 * @return true, if there was at least one removal
+	 */
+	bool RemoveAll(const FItemStack& Stack);
 	
 protected:
+
+	//Current weight of this container
+	float Weight;
+
+	//Current occupied slots
+	uint32 Num;
+
+	virtual bool AddStack_Impl(FItemStack& Stack);
+
+	/**Finds next index where item stack is not full
+	 * @param Stack to compare
+	 * @param Index Index to start from
+	 * @return Index of this slot, or -1, if no slot found
+	 */
+	uint32 NextIndex(const FItemStack& Stack, uint32 Index = 0) const;
+
+	/**Finds next index where item stack is empty
+	 * @param Index Index to start from
+	 * @return Index of this slot, or -1, if no slot found
+	 */
+	uint32 NextFreeIndex(uint32 Index = 0) const;
+
+	void FillFree(FItemStack& Stack);
+
+	void FillPartial(FItemStack& Stack);
+	
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
-	//Array of items
+	//Items array
+	UPROPERTY(EditInstanceOnly, Category="Container")
 	TArray<FItemStack> Inventory;
 
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
+	//Get read - only array of items
 	const TArray<FItemStack>& GetInventoryArray() const;
 
 };
