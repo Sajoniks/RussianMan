2f45fa05a8850d097cf2cae0f1112ee0f2580ce3
Sajoniks <avesom33@mail.ru>
2020-05-23 17:28:18 +0300

    Initial container commit

---
+++ Source/RussianMan_Game/Public/Actor/Interactable/Container.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Container.generated.h"
+
+UCLASS()
+class RUSSIANMAN_GAME_API AContainer : public AActor
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, Category="Inventory")
+	class UInventoryComponent* InventoryComponent;
+
+	UPROPERTY(EdtiAnywhere, Category="Visual", Instanced)
+	class USkeletalMeshComponent* WorldMesh;
+	
+public:	
+	// Sets default values for this actor's properties
+	AContainer();
+
+	UFUNCTION(BlueprintCallable, Category="Get")
+	class UInventoryComponent* GetInventory() const;
+	
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
