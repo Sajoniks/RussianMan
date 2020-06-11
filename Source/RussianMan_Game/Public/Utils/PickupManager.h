dc350f1064b8cddd70067c52a5a17b646df110c3
Sajoniks <avesom33@mail.ru>
2020-06-11 12:43:17 +0300

    Pickup manager class

---
+++ Source/RussianMan_Game/Public/Utils/PickupManager.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "PickupManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RUSSIANMAN_GAME_API UPickupManager : public UObject
+{
+	GENERATED_BODY()
+	
+};
